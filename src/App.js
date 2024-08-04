d4456edd9b9e69f8a10f9822ca4f9784d4c6b93a
Alan <alanacevedo2001@hotmail.com>
2024-08-04 12:37:26 -0300

    Revert "First Commit"
    This reverts commit aa71eea8da8b3029958f295409a9b6920de02f6a.

--- src/App.js
+++ src/App.js
@@ -1,54 +1,19 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
-import React, { lazy, Suspense, useState } from 'react';
-// import Home from "./pages/Home/Home"
-// import Landing from './pages/Landing/Landing';
-// import Carta from './pages/Carta/Carta';
-import Test from './pages/test/test';
-// import Detalles1 from './pages/Detalles1/Detalles1';
-import LoadingScreen from './pages/LoadingScreen';
-
-const Home = lazy(() => import('./pages/Home/Home'));
-const Landing = lazy(() => import('./pages/Landing/Landing'));
-const Carta = lazy(() => import('./pages/Carta/Carta'));
-const Detalles1 = lazy(() => import('./pages/Detalles1/Detalles1'));
-
+import Home from "./pages/Home/Home"
 function App() {
-    document.title = "Harry Potter";
-
-    return (
-        <div className="App">
-            <Suspense fallback={<LoadingScreen />}>
-                <Routes>
-                    <Route
-                        excact
-                        path="/home"
-                        element={<Home />}
-                    />
-                    <Route
-                        excact
-                        path="/"
-                        element={<Landing />}
-                    />
-                    <Route
-                        excact
-                        path="/carta"
-                        element={<Carta />}
-                    />
-                    <Route
-                        excact
-                        path="/test"
-                        element={<Test />}
-                    />
-                    <Route
-                        excact
-                        path="/detalles"
-                        element={<Detalles1 />}
-                    />
-                </Routes>
-            </Suspense>
-        </div>
-    );
+document.title="Taylor Swift - The Eras Tour"
+  return (
+    <div className="App">
+        <Routes>
+            <Route 
+                excact
+                path="/"
+                element={<Home />}
+            />
+        </Routes>
+    </div>
+  );
 }
 
 export default App;
